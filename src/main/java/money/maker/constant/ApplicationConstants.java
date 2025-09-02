8d88918a10e28438c6d2c5ddac2730d97f90be29
Gaurav Kumar <gauravkumar@CTIPL-MAC-278.local>
2025-09-02 22:19:42 +0530

    redis changes and bar aggregator service created

--- src/main/java/money/maker/constant/ApplicationConstants.java
+++ src/main/java/money/maker/constant/ApplicationConstants.java
@@ -4,8 +4,10 @@ public class ApplicationConstants {
 
     private ApplicationConstants() {}
 
+    public static final String NEW = "new";
+    public static final String TICK = "tick";
     public static final String BAR = "bar";
     public static final String DATA = "data";
     public static final String TIMESTAMP = "timestamp";
-
+    public static final String DELIMITER_DOT = ".";
 }
--- src/main/java/money/maker/service/RedisService.java
+++ src/main/java/money/maker/service/RedisService.java
@@ -3,11 +3,15 @@ package money.maker.service;
 import org.ta4j.core.Bar;
 import org.ta4j.core.BarSeries;
 
+import java.time.ZonedDateTime;
+
 public interface RedisService {
+
+    String getNewBarData(String instrument, ZonedDateTime timestamp);
+
+    String getNewTickData(String instrument, ZonedDateTime timestamp);
 
     BarSeries initInstrument(String instrument, int barCount);
 
     void updateInstrument(String instrument, Bar bar);
-
-    void onNewBarUpdate(String instrument, String timestamp);
 }
