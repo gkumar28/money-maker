f2c79342ea149e8777884e37a6a22f2fc11f4376
Gaurav Kumar <gkumar28@users.noreply.github.com>
2025-09-04 15:47:44 +0530

    capturing latest trading price of each instrument

--- data-emitter/src/main/java/data/emitter/constant/ApplicationConstants.java
+++ data-emitter/src/main/java/data/emitter/constant/ApplicationConstants.java
@@ -4,10 +4,6 @@ public class ApplicationConstants {
 
     private ApplicationConstants() {}
 
-    public static final String NEW = "new";
-    public static final String TICK = "tick";
-    public static final String BAR = "bar";
-    public static final String DATA = "data";
-    public static final String TIMESTAMP = "timestamp";
     public static final String DELIMITER_DOT = ".";
+    public static final String PRICE = "price";
 }
