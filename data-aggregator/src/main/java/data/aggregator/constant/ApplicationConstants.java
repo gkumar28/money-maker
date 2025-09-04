34cbb7f337417a2626ecd0cca238f9afc99c0323
Gaurav Kumar <gkumar28@users.noreply.github.com>
2025-09-04 15:48:26 +0530

    running aggregator redis operations in transaction

--- data-aggregator/src/main/java/data/aggregator/constant/ApplicationConstants.java
+++ data-aggregator/src/main/java/data/aggregator/constant/ApplicationConstants.java
@@ -10,4 +10,5 @@ public class ApplicationConstants {
     public static final String DATA = "data";
     public static final String TIMESTAMP = "timestamp";
     public static final String DELIMITER_DOT = ".";
+    public static final Integer REDIS_MAX_BAR_COUNT = 50;
 }
--- data-aggregator/src/main/java/data/aggregator/service/impl/RedisServiceImpl.java
+++ data-aggregator/src/main/java/data/aggregator/service/impl/RedisServiceImpl.java
@@ -3,15 +3,19 @@ package data.aggregator.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import data.aggregator.service.RedisService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.ta4j.core.Bar;
 
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.util.Set;
 
 import static data.aggregator.constant.ApplicationConstants.BAR;
 import static data.aggregator.constant.ApplicationConstants.DATA;
+import static data.aggregator.constant.ApplicationConstants.REDIS_MAX_BAR_COUNT;
 import static data.aggregator.constant.ApplicationConstants.TIMESTAMP;
 
 @Service
@@ -27,10 +31,27 @@ public class RedisServiceImpl implements RedisService {
         String dataKey = getKey(DATA, instrument);
         String timestamp = String.valueOf(bar.getEndTime().toInstant().toEpochMilli());
         String value = toCsvString(bar);
-        redisTemplate.opsForHash().put(dataKey, timestamp, value);
-
         String timestampKey = getKey(TIMESTAMP, instrument);
-        redisTemplate.opsForZSet().add(timestampKey, timestamp, Instant.now().toEpochMilli());
+
+        redisTemplate.execute(new SessionCallback<Void>() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public <K, V> Void execute(RedisOperations<K, V> operations) throws DataAccessException {
+                operations.multi();
+                operations.opsForHash().put((K) dataKey, timestamp, value);
+                operations.opsForZSet().add((K) timestampKey, (V) timestamp, -Instant.now().toEpochMilli());
+                operations.exec();
+
+                Set<V> oldTimestamps = operations.opsForZSet().range((K) timestampKey, REDIS_MAX_BAR_COUNT, -1);
+                if (null != oldTimestamps && !oldTimestamps.isEmpty()) {
+                    operations.opsForHash().delete((K) dataKey, oldTimestamps);
+                    operations.opsForZSet().remove((K) timestampKey, oldTimestamps);
+                }
+
+                return null;
+            }
+        });
+
 
         String channel = String.format("new.bar.%s", instrument);
         log.debug("sending bar event for instrument {} : {} {}", instrument, timestamp, value);
