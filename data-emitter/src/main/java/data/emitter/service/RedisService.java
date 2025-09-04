c7382a3f02bf6fb576e3ebd549bde1a55289c0cc
Gaurav Kumar <gauravkumar@CTIPL-MAC-278.local>
2025-09-04 15:47:44 +0530

    capturing latest trading price of each instrument

--- data-emitter/src/main/java/data/emitter/service/RedisService.java
+++ data-emitter/src/main/java/data/emitter/service/RedisService.java
@@ -3,4 +3,5 @@ package data.emitter.service;
 public interface RedisService {
 
     void raiseTickEvent(String instrument, String message);
+    void updateInstrumentPrice(String instrument, double price);
 }
--- data-emitter/src/main/java/data/emitter/service/impl/RedisServiceImpl.java
+++ data-emitter/src/main/java/data/emitter/service/impl/RedisServiceImpl.java
@@ -6,6 +6,9 @@ import data.emitter.service.RedisService;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import static data.emitter.constant.ApplicationConstants.DELIMITER_DOT;
+import static data.emitter.constant.ApplicationConstants.PRICE;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -18,5 +21,14 @@ public class RedisServiceImpl implements RedisService {
         log.debug("sending tick event for instrument {} : {}", instrument, message);
         String channel = String.format("new.tick.%s", instrument);
         redisTemplate.convertAndSend(channel, message);
+    }
+
+    @Override
+    public void updateInstrumentPrice(String instrument, double price) {
+        redisTemplate.opsForValue().set(getKey(PRICE, instrument), String.format("%.2f", price));
+    }
+
+    private String getKey(String keySpace, String key) {
+        return String.join(DELIMITER_DOT, keySpace, key);
     }
 }
