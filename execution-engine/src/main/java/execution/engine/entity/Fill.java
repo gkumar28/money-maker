4f5db61adda27fc43ff36fdaebe945110dc4c29a
Gaurav Kumar <gauravkumar@192.168.1.11>
2025-09-05 23:25:56 +0530

    execution-engine basic working model

--- execution-engine/src/main/java/execution/engine/entity/Fill.java
+++ execution-engine/src/main/java/execution/engine/entity/Fill.java
@@ -7,12 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "fills")
+@Data
+@NoArgsConstructor
 public class Fill {
 
     @Id
@@ -23,10 +27,10 @@ public class Fill {
     private Order order;
 
     @Column(precision = 19, scale = 8)
-    private BigDecimal filledQuantity;
+    private BigDecimal quantity;
 
     @Column(precision = 19, scale = 8)
-    private BigDecimal filledPrice;
+    private BigDecimal price;
 
     @Column
     private OffsetDateTime filledAt;
