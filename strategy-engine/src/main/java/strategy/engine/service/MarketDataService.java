5e8c9a7c57bec73d91505fb46e246e92cc4d2ae9
Gaurav Kumar <gkumar28@users.noreply.github.com>
2025-09-13 02:09:45 +0530

    changin logging level for rules and adding some custom functions for marketDataService & backtestService

--- strategy-engine/src/main/java/strategy/engine/service/MarketDataService.java
+++ strategy-engine/src/main/java/strategy/engine/service/MarketDataService.java
@@ -2,12 +2,17 @@ package strategy.engine.service;
 
 import org.ta4j.core.BarSeries;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
 public interface MarketDataService {
 
+    BarSeries loadHistoricalData(String instrument, LocalDate fromDate, LocalDate toDate);
+
     BarSeries loadHistoricalData(String instrument, String fromDate, String toDate);
+
+    List<Map<String, Object>> loadRawData(String instrument, LocalDate fromDate, LocalDate toDate);
 
     List<Map<String, Object>> loadRawData(String instrument, String fromDate, String toDate);
 }
--- strategy-engine/src/main/java/strategy/engine/service/impl/BacktestServiceImpl.java
+++ strategy-engine/src/main/java/strategy/engine/service/impl/BacktestServiceImpl.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Service;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.Trade;
 import org.ta4j.core.TradingRecord;
+import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
 import org.ta4j.core.num.DecimalNum;
 import org.ta4j.core.reports.TradingStatement;
 import org.ta4j.core.reports.TradingStatementGenerator;
 import strategy.engine.component.TradingStrategyFactory;
 import strategy.engine.constant.enums.StrategyType;
+import strategy.engine.indicator.KallmanIndicator;
 import strategy.engine.schemaobject.SignalDto;
 import strategy.engine.schemaobject.StrategyOrderDto;
 import strategy.engine.schemaobject.analysis.ExtendedTradeExecutionModel;
@@ -24,6 +26,12 @@ import strategy.engine.service.PositionManagementService;
 import strategy.engine.strategy.TradingStrategy;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static strategy.engine.util.StrategyEngineUtils.asTradeType;
 
@@ -59,6 +67,25 @@ public class BacktestServiceImpl implements BacktestService {
         }
 
         return new TradingStatementGenerator().generate(null, tradingRecord, barSeries);
+    }
+
+    @Override
+    public List<Map<String, Object>> getIndicatorValues(String instrument, LocalDate fromDate, LocalDate toDate) {
+        BarSeries barSeries = marketDataService.loadHistoricalData(instrument, fromDate, toDate);
+        barSeries.setMaximumBarCount(barSeries.getBarCount());
+        ClosePriceIndicator close = new ClosePriceIndicator(barSeries);
+        KallmanIndicator kallman = new KallmanIndicator(close, 0.1, 5);
+        List<Map<String, Object>> result = new ArrayList<>();
+        for (int i=0;i< barSeries.getBarCount();i++) {
+            Map<String, Object> data = new HashMap<>();
+            data.put("closePrice", close.getValue(i).bigDecimalValue());
+            data.put("kallmanPrice", kallman.getValue(i).bigDecimalValue());
+            data.put("% value", close.getValue(i).bigDecimalValue().divide(kallman.getValue(i).bigDecimalValue(), 2, RoundingMode.HALF_UP));
+
+            result.add(data);
+        }
+
+        return result;
     }
 
 
