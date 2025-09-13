17a16a21dca071b5a702f7995ba07bfea0787053
Gaurav Kumar <gauravkumar@192.168.1.4>
2025-09-14 01:44:03 +0530

    fixing execution-engin startup

--- execution-engine/src/main/java/execution/engine/ExecutionEngineApplication.java
+++ execution-engine/src/main/java/execution/engine/ExecutionEngineApplication.java
@@ -1,7 +1,21 @@
 package execution.engine;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
+@EntityScan(basePackages = "execution.engine.entity")
+@EnableJpaRepositories(basePackages = "execution.engine.repository")
+
 public class ExecutionEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExecutionEngineApplication.class, args);
+    }
 }
--- execution-engine/src/main/java/execution/engine/repository/FillRepository.java
+++ execution-engine/src/main/java/execution/engine/repository/FillRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FillRepository extends JpaRepository<Long, Fill> {
+public interface FillRepository extends JpaRepository<Fill, Long> {
 }
